3f5a40fb081ab38a25028f2d4b973a5ccef2bcca
Antonio Fuček <fucekantonio@gmail.com>
2026-02-13 19:10:44 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -526,7 +526,7 @@ CRITICAL RULES:
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, delay: 0.5 }}
+                transition={{ duration: 0.8, delay: 0.3 }}
                 className="w-full max-w-lg"
               >
                 <ChatInput onSend={handleSend} voiceMode={voiceMode} pauseListening={isSpeaking} />
